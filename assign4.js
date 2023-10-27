f902c266b547c2f7425f19e22a246c1163b8b8d0
Hadikatul Jannat <jhadikatul@gmail.com>
2023-10-28 00:51:21 +0600

    prblm 4

--- assign4.js
+++ assign4.js
@@ -53,3 +53,14 @@ function sortMaker(arr) {
 
 console.log(sortMaker([12,12]));
 
+function findAddress(obj) {
+    if (typeof obj !== 'object') {
+        return 'Provide a valid object.' ;
+    }
+    else {
+        const result = (obj.street || "_") +","+(obj.house || "_")+","+(obj.society || "_");
+
+        return result;
+    }
+  }
+
