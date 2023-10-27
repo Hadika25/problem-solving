05d1038417fb43e4300dc6ba906cad0e113636b6
Hadikatul Jannat <jhadikatul@gmail.com>
2023-10-28 00:48:13 +0600

    prblm2

--- assign4.js
+++ assign4.js
@@ -13,3 +13,19 @@ function cubeNumber(number) {
 const value = 3;
 const cube = cubeNumber(value);  
 console.log(cube);
+
+function matchFinder(string1, string2) {
+    if (typeof string1 !== "string" || typeof string2 !== "string") {
+        return "Input is invalid";
+    }
+    else {
+        result = string1.includes(string2);
+    }
+    return result;
+}
+
+const string1 = "JavaScript";
+const string2 = 8;
+
+const results = matchFinder(string1, string2);
+console.log(results);
